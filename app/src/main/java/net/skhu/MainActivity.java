e89387933ac8ea84e5684c951ef377bcbd0b2131
Kim, JeongHwan <jhkim980214@gmail.com>
2023-04-05 13:50:17 +0900

    SpinnersActivity

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -38,12 +38,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             return true;
         } else if (id == R.id.action_checkboxes) {
-        Intent intent = new Intent(this, CheckboxesActivity.class);
-        startActivity(intent);
-        return true;
-    }
-
-
+            Intent intent = new Intent(this, CheckboxesActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_spinners) {
+            Intent intent = new Intent(this, SpinnersActivity.class);
+            startActivity(intent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
